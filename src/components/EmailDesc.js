940e1fb10fcc9237626f74c24978485ee478e687
chinni00 <chinni0437@gmail>
2023-07-01 18:33:14 +0530

    when click on mail it opens entire message

---
+++ src/components/EmailDesc.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+
+
+const EmailDesc = () => {
+   const {id} = useParams()
+   const numId = Number(id)
+   
+   const items = useSelector(state=>state.emails)
+
+   const selectedMail = items.filter((item) => item.id === numId);
+
+   
+  return (
+   <center>
+     <div className="card w-50 shadow mt-5" style={{minHeight:'70vh'}}>
+      <div className="b-5 mb-4">
+        <p className="m-2" ><u>Sent to:</u></p>
+        <span className="">{selectedMail[0].to}</span>
+      </div>
+      <hr></hr>
+      <div className="b-5 ">
+        <p className="m-2" ><u>Composed Mail:</u></p>
+        <p className=" mt-5">{selectedMail[0].composedMail}</p>
+      </div>
+    </div>
+   </center>
+  );
+};
+
+export default EmailDesc;
