9690d82347a8d5b28be7afd8f2f99b4b8555ff19
chinni00 <chinni0437@gmail>
2023-07-03 00:51:22 +0530

    added a deleted handler that delete the email from outbox

--- src/components/Outbox.js
+++ src/components/Outbox.js
@@ -1,35 +1,62 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { useSelector } from 'react-redux'
+import React from "react";
+import Navbar from "./Navbar";
+import { useSelector ,useDispatch} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import MainNav from "./MainNav";
+import { outboxActions } from "../store";
+
 
 const Outbox = () => {
-  const emails = useSelector((state)=>state.emails)
-  console.log('outbox',emails)
+  const emails = useSelector((state) => state.emails);
+  console.log("outbox", emails);
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const deleteHandler =(id)=>{
+     const filteredEmails = emails.filter(item=>item.id!==id);
+     dispatch(outboxActions.storeDataInEmails(filteredEmails))
+     fetch('https://email-client-9f987-default-rtdb.firebaseio.com/email.json',{
+      method:'PUT',
+      body:JSON.stringify(filteredEmails),
+      headers:{
+        'Content-Type':'application/json'
+      }
+     }).then(res=>res.json()).catch(err=>console.log(err))
+  }
   return (
     <>
-    <Navbar />
-     <center className='w-75 float-end mt-3'>
-        <div className='card me-3 pt-2' style={{minHeight:'90vh'}}>
-          {
-            emails.map(email=>
-              <div className='card m-3 p-4 mb-4'>
-                 <div className='b-5'>
-                  <b className='m-5'>Email:</b><span className=''>{email.to}</span>
-                 </div>
-                 <hr></hr>
-                 <div>
-                  <p className=' fw-bold'>Composed Mail:</p><span className=''>{email.composedMail}</span>
-                 </div>
-
+    <MainNav />
+      <Navbar />
+      {emails == null && emails == undefined && <p>No mails sent</p>}
+      {emails && (
+        <center className="w-75 float-end mt-3">
+          
+          <div>Welcome to Mail Box</div>
+          <hr></hr>
+          <div className="card me-3 pt-2" style={{ minHeight: "90vh" }}>
+          <h3>Emails</h3>
+            {emails.map((email) => (
+              <div
+                className="card m-3 p-3 mb-4"
+                key={email.id}
+                style={{ cursor: "pointer" }}
+                 
+              >
+                <div className="b-5 d-flex justify-content-around align-items-center">
+                  <div className="b-5"  onClick={()=>{navigate(`/emailDesc/${email.id}`)}}>
+                    <b className="m-5">Email:</b>
+                    <span className="" >{email.to}</span>
+                  </div>
+                  <button className="btn btn-success" onClick={()=>{deleteHandler(email.id)}}>Delete</button>
+                </div>
               </div>
-              )
-          }
-        </div>
-    </center>
+            ))}
+          </div>
+        </center>
+      )}
     </>
-   
-    
-  )
-}
+  );
+};
 
-export default Outbox
+export default Outbox;
