7e420f5b680b519728dba6add0cb8bf44554584e
chinni00 <chinni0437@gmail>
2023-07-03 16:58:56 +0530

    created router for inbox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp';
+import Welcome from './components/Welcome';
+import Outbox from './components/Outbox';
+import { useEffect, useState } from 'react';
+import EmailDesc from './components/EmailDesc';
+import Inbox from './components/Inbox';
+import InboxDesc from './components/InboxDesc';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
 
 function App() {
+
+ let token = useSelector(state=>state.token)
+
+  
   return (
     <div className="App">
-      <SignUp />
+     <Routes>
+      <Route path='*' element={<SignUp />} />
+       <Route path='/' element={<SignUp />} />
+       <Route path='/inbox' element={<Inbox />} />
+       <Route path='/inboxDesc/:id' element={<InboxDesc />} />
+   { token!==null && token &&  <Route path ='/outbox' element={<Outbox />} />}
+      <Route path='/welcome' element={<Welcome />} />
+   {  token!==null &&  <Route path='/emailDesc/:id' element={<EmailDesc />} />}
+     </Routes>
     </div>
   );
 }
