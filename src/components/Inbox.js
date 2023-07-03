701b0085c92f7933d710812c8c33515fc53b373c
chinni00 <chinni0437@gmail>
2023-07-03 16:54:40 +0530

    created inbox for reading inbox messages

---
+++ src/components/Inbox.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import MainNav from './MainNav'
+import Navbar from './Navbar'
+import { outboxActions } from '../store'
+import { useNavigate } from 'react-router-dom'
+import InboxItem from './InboxItem'
+
+
+const Inbox = () => {
+   
+  const myMail = useSelector(state=>state.myMail)
+  const myUserName = myMail.split('@')[0]
+  const inbox = useSelector(state=>state.inBox)
+  const dispatch= useDispatch();
+  const navigate = useNavigate()
+
+   const fetchInbox =()=>{
+    fetch(`https://email-client-9f987-default-rtdb.firebaseio.com/inbox/${myUserName}.json`).then(res=>res.json())
+    .then(data=> {
+        dispatch(outboxActions.setInbox(data))
+    }).catch(err=>console.log(err))
+   } 
+   useEffect(()=>{
+     fetchInbox();
+   },[dispatch])
+   console.log('inbox',inbox)
+
+   const deleteHandler =(id)=>{
+    const filteredEmails = inbox.filter(item=>item.id!==id);
+    dispatch(outboxActions.setInbox(filteredEmails))
+    fetch(`https://email-client-9f987-default-rtdb.firebaseio.com/inbox/${myUserName}.json`,{
+     method:'PUT',
+     body:JSON.stringify(filteredEmails),
+     headers:{
+       'Content-Type':'application/json'
+     }
+    }).then(res=>res.json()).catch(err=>console.log(err))
+ }
+
+ 
+  return (
+    <>
+    <MainNav />
+    <Navbar />
+    <div>
+        <h1>Inbox</h1>
+        {inbox == null && inbox == undefined && <p>No mails sent</p>}
+      { inbox && (
+        <center className="w-75 float-end mt-3">
+          <hr></hr>
+          <div className="card me-3 pt-2" style={{ minHeight: "90vh" }}>
+          <h3>Emails</h3>
+            {inbox.map((email) => (
+              <div
+                className="card m-3 p-3 mb-4"
+                key={email.id}
+                style={{ cursor: "pointer" }}
+                 
+              >
+                <div className="b-5 d-flex justify-content-around align-items-center">
+                  <div className="b-5" >
+                    <InboxItem from={email.from} id={email.id}  />
+                  </div>
+                  <button className="btn btn-success" onClick={()=>{deleteHandler(email.id)}} >Delete</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </center>
+      )}
+    </div>
+    </>
+  )
+}
+
+export default Inbox
